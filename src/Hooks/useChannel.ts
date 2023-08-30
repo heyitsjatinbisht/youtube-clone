4bb804a0eb097b3704f007fa131c4c6e55f848e5
Jsbist21 <bishtjatin477@gmail.com>
2023-08-30 10:34:53 +0530

    Updated page title

--- src/Hooks/useChannel.ts
+++ src/Hooks/useChannel.ts
@@ -1,47 +1,47 @@
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
+// import { useState, useEffect } from "react";
+// import apiClient from "../services/api-client";
 
-interface ChannelResponse {
-  items: FetchChannel[];
-}
-export interface FetchChannel {
-  snippet: ChannelSnippet;
-  id: string;
-}
-interface ChannelSnippet {
-  thumbnails: ChannelThumbnail;
-}
+// interface ChannelResponse {
+//   items: FetchChannel[];
+// }
+// export interface FetchChannel {
+//   snippet: ChannelSnippet;
+//   id: string;
+// }
+// interface ChannelSnippet {
+//   thumbnails: ChannelThumbnail;
+// }
 
-interface ChannelThumbnail {
-  default: ChannelThumbnailSize;
-  medium: ChannelThumbnailSize;
-}
-interface ChannelThumbnailSize {
-  url: string;
-  width: number;
-  height: number;
-}
+// interface ChannelThumbnail {
+//   default: ChannelThumbnailSize;
+//   medium: ChannelThumbnailSize;
+// }
+// interface ChannelThumbnailSize {
+//   url: string;
+//   width: number;
+//   height: number;
+// }
 
-const useChannel = () => {
-  const [channelAvatar, setChannelAvatar] = useState<FetchChannel[]>([]);
-  const [error, setError] = useState("");
+// const useChannel = () => {
+//   const [channelAvatar, setChannelAvatar] = useState<FetchChannel[]>([]);
+//   const [error, setError] = useState("");
 
-  useEffect(() => {
-    apiClient
-      .get<ChannelResponse>("/channels", {
-        params: {
-          part: "id",
-          managedByMe: true,
-          maxResults: 15,
-          mine: true,
-          mySubscribers: true,
-          key: import.meta.env.VITE_REACT_APP_API_KEY,
-        },
-      })
-      .then((response) => setChannelAvatar(response.data.items))
-      .catch((error) => setError(error.message));
-  }, []);
-  return { channelAvatar, error };
-};
+//   useEffect(() => {
+//     apiClient
+//       .get<ChannelResponse>("/channels", {
+//         params: {
+//           part: "id",
+//           managedByMe: true,
+//           maxResults: 15,
+//           mine: true,
+//           mySubscribers: true,
+//           key: import.meta.env.VITE_REACT_APP_API_KEY,
+//         },
+//       })
+//       .then((response) => setChannelAvatar(response.data.items))
+//       .catch((error) => setError(error.message));
+//   }, []);
+//   return { channelAvatar, error };
+// };
 
-export default useChannel;
+// export default useChannel;
--- src/components/ChannelAvatar.tsx
+++ src/components/ChannelAvatar.tsx
@@ -1,20 +1,20 @@
-// import Avatar from "@mui/material/Avatar";
-import useChannel from "../Hooks/useChannel";
-import { Box } from "@mui/material";
+// // import Avatar from "@mui/material/Avatar";
+// import useChannel from "../Hooks/useChannel";
+// import { Box } from "@mui/material";
 
-const ChannelAvatar = () => {
-  const { channelAvatar } = useChannel();
+// const ChannelAvatar = () => {
+//   const { channelAvatar } = useChannel();
 
-  return (
-    <>
-      {channelAvatar.map((ch) => (
-        <Box key={ch.id}>
-          {/* <Avatar src={ch.snippet.thumbnails.default.url} /> */}
-          <img src={ch.snippet.thumbnails.default.url}></img>
-        </Box>
-      ))}
-    </>
-  );
-};
+//   return (
+//     <>
+//       {channelAvatar.map((ch) => (
+//         <Box key={ch.id}>
+//           {/* <Avatar src={ch.snippet.thumbnails.default.url} /> */}
+//           <img src={ch.snippet.thumbnails.default.url}></img>
+//         </Box>
+//       ))}
+//     </>
+//   );
+// };
 
-export default ChannelAvatar;
+// export default ChannelAvatar;
